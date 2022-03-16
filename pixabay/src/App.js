d2f61b690dffd6f9fb86d6f3c925be997a1de57b
msiroilem <twjones86@gmail.com>
2022-03-16 14:16:19 -0400

    setting up axios call to get Pixabay images

--- pixabay/src/App.js
+++ pixabay/src/App.js
@@ -1,12 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 import Search from '../src/components/Search'
+import { BASE_URL, CONFIG } from '../src/globals'
 
-function App() {
+export default function App() {
+  const [images, setImages] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const [searched, toggleSearched] = useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
+
+  useEffect(() => {
+    if (!searched) {
+      getImages()
+    }
+  }, [])
+
+  const getImages = async () => {
+    const res = await axios.get(`${BASE_URL}`)
+    setImages(res.data.results)
+  }
+
+  const getSearchResults = async (e) => {
+    e.preventDefault()
+    const res = await axios.get(`${BASE_URL}`)
+    setSearchResults(res.data.results)
+    toggleSearched(true)
+  }
+
+  const handleChange = async (e) => {
+    setSearchQuery(e.target.value)
+  }
+
   return (
     <div className="App">
-      <Search />
+      <Search onChange={handleChange} onSubmit={getSearchResults} />
     </div>
   )
 }
-
-export default App
---
+++ pixabay/src/globals.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://pixabay.com/api/'
+
+export const CONFIG = `${process.env.REACT_APP_PIXABAY_KEY}`
